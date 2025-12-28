e1a8a8850a24bacbd50b2a3f2516ca1a16f22140
Rajesh Das <rajesh@hyperoot.dev>
2025-12-28 13:53:49 +0000

    chore: update enter_project.sh to use uv run for commands

--- scripts/enter_project.sh
+++ scripts/enter_project.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if ! command -v cz >/dev/null; then
-  pip install --user pipx
-  pipx install commitizen
+  uv run pip install --user pipx
+  uv run pipx install commitizen
 fi
 
 if [ ! -f .git/hooks/pre-commit ]; then
-  pre-commit install
+  uv run pre-commit install
 fi
 
 if [ ! -f .git/hooks/commit-msg ]; then
-  pre-commit install --hook-type commit-msg
+  uv run pre-commit install --hook-type commit-msg
 fi
